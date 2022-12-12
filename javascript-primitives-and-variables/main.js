bcf8e5002903bc6a2b63582fffc36471b811b196
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-12 18:33:13 +0000

    Completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,21 @@
+var fullName = 'Heerak Moon';
+var isCool = false;
+var totalPets = 4;
+
+console.log('fullName value:', fullName);
+console.log('isCool value:', isCool);
+console.log('totalPets value:', totalPets);
+
+console.log('typeof fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+
+console.log('unknown value:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+
+console.log('never value:', never);
+console.log('typeof never:', typeof never);
