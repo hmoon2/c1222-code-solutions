224f9e54d9b88519b5a4f46ae90d128c8d47bf1b
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-14 19:29:11 +0000

    first two problems

---
+++ javascript-if/main.js
@@ -0,0 +1,25 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro
+ */
+function isUnderFive(number) {
+  if (number < 5) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isEven(number) {
+  if (number % 2) {
+    return true;
+  } else {
+    return false;
+  }
+
+}
