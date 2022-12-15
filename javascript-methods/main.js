392508e5f1f445e1dd1a4162176af0dca02f9051
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-15 00:40:26 +0000

    fix randomHero to be random each refresh

---
+++ javascript-methods/main.js
@@ -0,0 +1,16 @@
+var num1 = 3;
+var num2 = 5;
+var num3 = 8;
+var maximumValue = Math.max(num1, num2, num3);
+
+console.log('maxiumumValue:', maximumValue);
+
+var heroes = ['Spiderman', 'Iron Man', 'Black Panther', 'Thor'];
+var randomNumber = Math.random(heroes.length);
+var randomIndex = Math.floor(randomNumber);
+
+console.log('randomIndex:', randomIndex);
+
+var randomHero = heroes[randomIndex];
+
+console.log('randomHero:', randomHero);
