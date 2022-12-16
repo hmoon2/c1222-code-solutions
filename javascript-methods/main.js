2dcaf92e142e60fddf6dc3c49ed738104fbe84ee
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-16 10:36:38 +0000

    able to console.log randomIndex and randomHero

--- javascript-methods/main.js
+++ javascript-methods/main.js
@@ -6,11 +6,13 @@ var maximumValue = Math.max(num1, num2, num3);
 console.log('maxiumumValue:', maximumValue);
 
 var heroes = ['Spiderman', 'Iron Man', 'Black Panther', 'Thor'];
-var randomNumber = Math.random(heroes.length);
+
+var randomNumber = Math.random();
+randomNumber *= heroes.length;
 var randomIndex = Math.floor(randomNumber);
 
-console.log('randomIndex:', randomIndex);
+console.log(randomIndex);
 
 var randomHero = heroes[randomIndex];
 
-console.log('randomHero:', randomHero);
+console.log(randomHero);
