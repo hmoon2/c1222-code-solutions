23947f4155174a5b159df5f420b12f1a0887a7be
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-16 03:49:06 +0000

    find getIndexes number not string

--- javascript-loops-practice/get-indexes.js
+++ javascript-loops-practice/get-indexes.js
@@ -3,7 +3,7 @@ function getIndexes(array) {
   var newArray = [];
   for (var i = 0; i < array.length; i++) {
     newArray.push(array[i]);
-    i++;
   }
   return newArray;
 }
+// needs to return index number not strings
