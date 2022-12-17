fc04ed3786763ff97cdbf94ab0136fdc48f5fb44
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-17 00:55:10 +0000

    work in progress getStudentNames

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -1,1 +1,8 @@
 /* exported getStudentNames */
+function getStudentNames(students) {
+  var studentNames = [];
+  for (var i = 0; i < students.length; i++) {
+    studentNames.push();
+  }
+  return studentNames;
+}
