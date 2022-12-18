c674b399133fea0c0d7e2f6654993ec12973e608
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-18 08:53:33 +0000

    wip: includesSeven(array)

--- javascript-loops-practice/includes-seven.js
+++ javascript-loops-practice/includes-seven.js
@@ -1,1 +1,10 @@
 /* exported includesSeven */
+function includesSeven(array) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === 7) {
+      return true;
+    } else if (array[i] !== 7) {
+      return false;
+    }
+  }
+}
