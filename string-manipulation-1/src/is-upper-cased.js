dbb3605eb35af5d2d7ff044b085e2321677823f9
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-19 11:08:49 +0000

    working on isUpperCase(word)

--- string-manipulation-1/src/is-upper-cased.js
+++ string-manipulation-1/src/is-upper-cased.js
@@ -1,1 +1,16 @@
 /* exported isUpperCased */
+/* create a function
+inside the function code block, assign the boolean true to a variable (wordIs)
+then create a for loop. Initialize with a variable equal to 0
+then have i be less than the length of the word and have i be incremented
+in the loop, if a letter can be changed to uppercase, if it can assign false to the variable wordIs
+return the value of the variable wordIs */
+function isUpperCased(word) {
+  var wordIs = true;
+  for (var i = 0; i < word.length; i++) {
+    if (word[i].toUpperCase) {
+      wordIs = false;
+    }
+  }
+  return wordIs;
+}
