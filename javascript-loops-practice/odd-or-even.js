365a5df9220f948d9608ea8b2d9cea193bec53a6
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-19 05:02:37 +0000

    Completed javascript-loops-practice

--- javascript-loops-practice/odd-or-even.js
+++ javascript-loops-practice/odd-or-even.js
@@ -1,1 +1,12 @@
 /* exported oddOrEven */
+function oddOrEven(numbers) {
+  var numbersAre = [];
+  for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+      numbersAre.push('even');
+    } else if (numbers[i] % 2 !== 0) {
+      numbersAre.push('odd');
+    }
+  }
+  return numbersAre;
+}
