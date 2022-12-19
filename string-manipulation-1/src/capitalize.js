2e4c8b5b18838be3280b1c1ea5d12c1d87a7e2c7
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-19 10:53:23 +0000

    wip: capitalize(word) done

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,1 +1,10 @@
 /* exported capitalize */
+/* create a function
+inside the code block, return word at index 0 and use the toUpperCase method
+then concatenate the letters that aren't capital with the slice method */
+function capitalize(word) {
+  var firstLetter = word[0].toUpperCase();
+  var restOfLetters = word.slice(1);
+  var lowerCase = restOfLetters.toLowerCase();
+  return firstLetter + lowerCase;
+}
