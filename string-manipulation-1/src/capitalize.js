fe774b73dc3ae24de114e7a2b715e3e9022908b3
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-19 10:54:22 +0000

    wip: capitalized(word) pseudo code

--- string-manipulation-1/src/capitalize.js
+++ string-manipulation-1/src/capitalize.js
@@ -1,7 +1,9 @@
 /* exported capitalize */
 /* create a function
-inside the code block, return word at index 0 and use the toUpperCase method
-then concatenate the letters that aren't capital with the slice method */
+inside the code block, make the first letter of the word uppercase and assign it to a variable (firstLetter)
+take the rest of the letters from the word and assign it to a separate variable (restOfLetters)
+use the toLowerCase method on restOfLetters variable and assign the value to a new variable (lowerCase)
+concatenate the firstLetter variable and the lowerCase variable and return the value out of the function */
 function capitalize(word) {
   var firstLetter = word[0].toUpperCase();
   var restOfLetters = word.slice(1);
