c6b10b115cab63567212d60d2338a1e20ba53048
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-19 10:08:28 +0000

    wip: getLastChar(string) done

--- string-manipulation-1/src/get-last-char.js
+++ string-manipulation-1/src/get-last-char.js
@@ -1,1 +1,6 @@
 /* exported getLastChar */
+/* create a function with one argument
+in the function code block, return the string at string.length - 1 */
+function getLastChar(string) {
+  return string[string.length - 1];
+}
