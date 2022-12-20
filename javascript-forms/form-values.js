f77debf34065d8c7703e41e2e647586948d52b16
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-20 18:49:33 +0000

    javascript-forms resubmit

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,19 +1,16 @@
 var contactForm = document.querySelector('#contact-form');
-var firstInputElement = document.querySelector('#user-name');
-var secondInputElement = document.querySelector('#user-email');
-var textAreaElement = document.querySelector('textarea');
 
 function submitEvent(event) {
   event.preventDefault();
-  var firstInputValue = firstInputElement.value;
-  var secondInputValue = secondInputElement.value;
-  var textAreaValue = textAreaElement.value;
+  var firstInputValue = contactForm.elements.name.value;
+  var secondInputValue = contactForm.elements.email.value;
+  var textAreaValue = contactForm.elements.message.value;
   var object = {};
   object.name = firstInputValue;
   object.email = secondInputValue;
   object.message = textAreaValue;
   console.log('messageData:', object);
-  document.querySelector('form').reset();
+  contactForm.reset();
 }
 
 contactForm.addEventListener('submit', submitEvent);
