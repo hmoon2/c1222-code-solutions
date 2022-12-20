da439caa6eadece1917498e21444a06ba9b26f85
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-20 00:43:47 +0000

    wip: dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,18 @@
+/* First, declare three variables. One for the number of times a user clicks the button, a second for the querySelector
+for the hot-button class, and a third for the querySelector for the click-count class. Create a function that will be
+used in the addEventListener method. In the function, when the 'click' event is fired then the counter variable will increment.
+Next, create an event listener for the button element. The arguments will be 'click' and the function that was created.
+udpate the function and have it update textContent every time it is clicked. Also, in the function, make it so that as the
+number of clicks go up the textContent changes.  */
+
+// var numberOfClicks = 0;
+// var clickHotButton = document.querySelector('.hot-button');
+// var clickCount = document.querySelector('.click-count');
+
+// function hotButton(clickHotButton) {
+//   numberOfClicks++;
+// }
+
+// console.log(clickCount);
+
+// hotButton.addEventListener('click', hotButton);
