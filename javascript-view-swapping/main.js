68e0918c9f146a3b7f1d22a9433b74a4dc4f2ad5
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-21 20:36:38 +0000

    wip: querySelector variables

---
+++ javascript-view-swapping/main.js
@@ -0,0 +1,3 @@
+// var classTabContainer = document.querySelector('.tab-container');
+// var tabNodeList = document.querySelectorAll('.tab');
+// var viewNodeList = document.querySelectorAll('.view');
