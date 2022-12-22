ea7acf8b8212e0100b434894738c7fda9415db06
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-22 00:06:40 +0000

    work in progress: for loop for tab nodelist

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,3 +1,19 @@
-// var classTabContainer = document.querySelector('.tab-container');
-// var tabNodeList = document.querySelectorAll('.tab');
+var classTabContainer = document.querySelector('.tab-container');
+var tabNodeList = document.querySelectorAll('.tab');
 // var viewNodeList = document.querySelectorAll('.view');
+
+function switchTabs(event) {
+  if (event.target.matches('.tab') === true) {
+    for (var i = 0; i < tabNodeList.length; i++) {
+      if (i === event.target) {
+        tabNodeList[i].className = 'tab active';
+      } else {
+        tabNodeList[i].className = 'tab';
+      }
+    }
+  }
+}
+
+classTabContainer.addEventListener('click', switchTabs);
+
+// console.log(tabNodeList);
