78beb917d3211c9eb2af5ebcf26eaed1bad7d1c5
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-25 23:45:49 +0000

    wip: tail(array) done

--- array-manipulation-1/src/tail.js
+++ array-manipulation-1/src/tail.js
@@ -1,16 +1,14 @@
 /* exported tail */
 /* create a function with one parameter (array) and in the function
-create a variable (tailArray) with an empty array as its value and then create a for loop
-in the for loop create an if statement
-the condition for the if statement being, if array at i is not equal to array at the first index, then push it into the empty array
-lastly return tailArray */
+create a variable (tailArray) with an empty array as its value
+then create a for loop, the loop will start with the second item in the array instead of the first
+during each iteration each items will be pushed into the tailArray
+the function should return tailArray */
 
 function tail(array) {
   var tailArray = [];
-  for (var i = 0; i < array.length; i++) {
-    if (array[i] !== array[0]) {
-      tailArray.push(array[i]);
-    }
+  for (var i = 1; i < array.length; i++) {
+    tailArray.push(array[i]);
   }
   return tailArray;
 }
