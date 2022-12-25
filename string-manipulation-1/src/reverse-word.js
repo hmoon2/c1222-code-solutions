69438ddaaf5e57ab68bd9068880592a086c11fc3
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-25 23:12:36 +0000

    Completed string-manipulation-1 exercise

--- string-manipulation-1/src/reverse-word.js
+++ string-manipulation-1/src/reverse-word.js
@@ -1,1 +1,14 @@
 /* exported reverseWord */
+/* create a function with one parameter (word)
+In the function create a variable with an empty string as its value (newWord)
+Create a for loop. With each iteration, take the last letter of the word and concatenate it with the empty string
+Return newWord
+ */
+
+function reverseWord(word) {
+  var newWord = '';
+  for (var i = word.length - 1; i >= 0; i--) {
+    newWord += word[i];
+  }
+  return newWord;
+}
