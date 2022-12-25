5a272689b474e50bbe8e1d420fb0d48b9d6922c8
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-25 22:50:00 +0000

    wip: string-manipulation-1 done

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,7 +1,13 @@
 /* exported getWords */
 /* create a function with one parameter (string)
- inside the function, create a new variable (wordsInString) have it equal  */
+ inside the function, create an if else statement
+ the condition being if the string is empty then the return is the string split at an empty string
+ else the return is the string split at every space */
 
 function getWords(string) {
-  return string.split(' ');
+  if (string === '') {
+    return string.split('');
+  } else {
+    return string.split(' ');
+  }
 }
