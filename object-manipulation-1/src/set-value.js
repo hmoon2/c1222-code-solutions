ad48955e70b01fdb46abf83b91a088855cd40333
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-26 07:21:23 +0000

    wip: setValue(object, key, value) done

--- object-manipulation-1/src/set-value.js
+++ object-manipulation-1/src/set-value.js
@@ -1,1 +1,7 @@
 /* exported setValue */
+/* create a function with three parameters (object, key, value)
+in the function assign value to the object at the key property */
+
+function setValue(object, key, value) {
+  object[key] = value;
+}
