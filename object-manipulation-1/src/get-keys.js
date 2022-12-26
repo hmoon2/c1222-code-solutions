7bb8e9ab1c15838d82913c6c5bc01c63ab84e3e7
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-26 07:34:44 +0000

    wip: getKeys(object) done

--- object-manipulation-1/src/get-keys.js
+++ object-manipulation-1/src/get-keys.js
@@ -1,1 +1,13 @@
 /* exported getKeys */
+/* create a function with one parameter (object)
+in the function code block create a variable (objProperties) and assign it an empty array
+then create a for in loop and in the for in loop's code block push the property key into the variable objProperties
+return objProperties */
+
+function getKeys(object) {
+  var objProperties = [];
+  for (var key in object) {
+    objProperties.push(key);
+  }
+  return objProperties;
+}
