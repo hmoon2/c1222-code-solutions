c008024519cb0e2e41b201d01297e63b90a7b484
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-26 02:22:33 +0000

    Completed array-manipulation-1 exercise

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -6,12 +6,12 @@ and create an if statement in the for loop, in the condition if the value is a t
 then push the item into the empty array (truthyOnlyArray)
 the return will be truthyOnlyArray */
 
-// function compact(array) {
-//   var truthyOnlyArray = [];
-//   for (var i = 0; i < array.length; i++) {
-//     if (array[i] === ) {
-//       truthyOnlyArray.push(array[i]);
-//     }
-//   }
-//   return truthyOnlyArray;
-// }
+function compact(array) {
+  var truthyOnlyArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      truthyOnlyArray.push(array[i]);
+    }
+  }
+  return truthyOnlyArray;
+}
