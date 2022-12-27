f5cacfcfd88aface66da81b9324c881e85adab1c
Heerak Moon <hmoon3@student.mtsac.edu>
2022-12-27 08:27:14 +0000

    Completed javascript-view-swapping exercise

--- javascript-view-swapping/main.js
+++ javascript-view-swapping/main.js
@@ -1,19 +1,25 @@
 var classTabContainer = document.querySelector('.tab-container');
 var tabNodeList = document.querySelectorAll('.tab');
-// var viewNodeList = document.querySelectorAll('.view');
+var viewNodeList = document.querySelectorAll('.view');
 
 function switchTabs(event) {
   if (event.target.matches('.tab') === true) {
     for (var i = 0; i < tabNodeList.length; i++) {
-      if (i === event.target) {
+      if (tabNodeList[i] === event.target) {
         tabNodeList[i].className = 'tab active';
       } else {
         tabNodeList[i].className = 'tab';
+      }
+    }
+    var dataViewValue = event.target.getAttribute('data-view');
+    for (var m = 0; m < viewNodeList.length; m++) {
+      if (viewNodeList[m].getAttribute('data-view') === dataViewValue) {
+        viewNodeList[m].className = 'view';
+      } else {
+        viewNodeList[m].className = 'view hidden';
       }
     }
   }
 }
 
 classTabContainer.addEventListener('click', switchTabs);
-
-// console.log(tabNodeList);
