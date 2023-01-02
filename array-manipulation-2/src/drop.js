b343d138704e82de76f448daa8ea2e56cc88e674
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-02 10:58:57 +0000

    in progress: drop(array, count) done

--- array-manipulation-2/src/drop.js
+++ array-manipulation-2/src/drop.js
@@ -1,1 +1,14 @@
 /* exported drop */
+/* create a function with two parameters (array, count)
+in the function create a variable with an empty array as its value
+then create a for loop, i will equal and start the loop at the number that is
+put into the count parameter. The item from array at i will then be pushed into
+the empty array and returned */
+
+function drop(array, count) {
+  var countArray = [];
+  for (var i = count; i < array.length; i++) {
+    countArray.push(array[i]);
+  }
+  return countArray;
+}
