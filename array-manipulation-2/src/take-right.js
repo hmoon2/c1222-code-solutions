07af848b18482fe616c0a5582ca92a58ec7f8d47
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-02 11:09:27 +0000

    working on takeRight(array, count)

--- array-manipulation-2/src/take-right.js
+++ array-manipulation-2/src/take-right.js
@@ -1,1 +1,12 @@
 /* exported takeRight */
+/* create a function with two parameters (array, count)
+in the function create a variable with an empty array as its value
+then create a for loop to iterate through the array, i will start at */
+
+function takeRight(array, count) {
+  var lastCountElementsArr = [];
+  for (var i = array.length - count; i < array.length; i++) {
+    lastCountElementsArr.push(array[i]);
+  }
+  return lastCountElementsArr;
+}
