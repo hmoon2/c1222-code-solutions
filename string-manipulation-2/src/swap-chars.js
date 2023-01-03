33bcfedeced34b9c582f4efcfbc42d2c8ba20b1f
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-03 09:28:29 +0000

    swapChars in progress

--- string-manipulation-2/src/swap-chars.js
+++ string-manipulation-2/src/swap-chars.js
@@ -1,1 +1,9 @@
 /* exported swapChars */
+
+function swapChars(firstIndex, secondIndex, string) {
+  var swappedCharsString = '';
+  string.split('');
+  [string[firstIndex], string[secondIndex]] = [string[secondIndex], string[firstIndex]];
+
+  return swappedCharsString;
+}
