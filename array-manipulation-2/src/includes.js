7864a3ca1dc7dd03ef02c97f52886cb76399bd73
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-03 10:33:48 +0000

    in progress: includes(array, value) completed

--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,1 +1,14 @@
 /* exported includes */
+/* create a function with two parameters (array, value)
+in the function, create a for loop and in that for loop create an if statement
+if the item at the i index of the array is strictly equal to the value then the return will be true
+otherwise the return will be false */
+
+function includes(array, value) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return true;
+    }
+  }
+  return false;
+}
