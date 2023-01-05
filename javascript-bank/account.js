a4500440dbe3e639311860a4b3e89fccfa3d7701
Heerak Moon <hmoon3@student.mtsac.edu>
2023-01-05 08:49:13 +0000

    deposit and withdraw prototype methods created

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,37 @@
 /* exported Account */
+
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+
+Account.prototype.deposit = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var newDeposit = new Transaction('deposit', amount);
+    this.transactions.push(newDeposit);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.withdraw = function (amount) {
+  if (Number.isInteger(amount) && amount > 0) {
+    var newWithdrawal = new Transaction('withdrawal', amount);
+    this.transactions.push(newWithdrawal);
+    return true;
+  } else {
+    return false;
+  }
+};
+
+Account.prototype.getBalance = function () {
+  var balance = 0;
+  if (Account.deposit) {
+    balance += this.transactions;
+  } else if (Account.withdraw) {
+    balance -= this.transactions;
+  }
+  return balance;
+};
